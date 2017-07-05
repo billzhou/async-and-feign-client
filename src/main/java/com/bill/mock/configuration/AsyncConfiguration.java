2c1a422dc920b3f517a03698b0e94aadc0c59e03
Guang Zhou <Guang.Zhou@Staples.com>
2017-07-05 15:50:23 -0400

    Add explain to spring asyn configuration parameters

--- src/main/java/com/bill/mock/configuration/AsyncConfiguration.java
+++ src/main/java/com/bill/mock/configuration/AsyncConfiguration.java
@@ -11,6 +11,16 @@ import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 
+/*
+ * https://docs.spring.io/spring/docs/current/spring-framework-reference/html/scheduling.html
+ * The main idea is that when a task is submitted, the executor will first try to use a free thread if the number of active threads is currently less than the core size. 
+ * If the core size has been reached, then the task will be added to the queue as long as its capacity has not yet been reached. Only then, 
+ * if the queue’s capacity has been reached, will the executor create a new thread beyond the core size. If the max size has also been reached, then the executor will reject the task.
+ * 
+ * keep-alive setting determines the time limit (in seconds) for which threads may remain idle before being terminated. 
+ * If there are more than the core number of threads currently in the pool, after waiting this amount of time without processing a task, excess threads will get terminated. 
+ */
+
 @EnableAsync
 @Configuration
 @ConfigurationProperties(prefix = "com.bill.async")
@@ -20,6 +30,7 @@ public class AsyncConfiguration {
 	
 	private int corePoolSize = 10;
 	private int maxPoolSize = 50;
+	//By default, the queue is unbounded, but this is rarely the desired configuration, because it can lead to OutOfMemoryErrors if enough tasks are added to that queue while all pool threads are busy.
 	private int queueCapacity = 1000;
 	private String threadNamePrefix = "Bill-Exec-";
 	
